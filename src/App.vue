250928ed7188c32f24120783027f39f5766ff883
Patrick FELIX <felix@labri.fr>
2020-04-17 15:55:46 +0200

    initbis

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
+<<<<<<< HEAD
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+=======
+
+  <div id="app">
+    <p>Un paragraphe 1</p>
+    <img alt="Vue logo" src="./assets/logo.png">
+        <p>Un paragraphe 2</p>
+    <Composant2 msgbis="Bonjour tout le monde !"/>
+    <HelloWorld msg="Hello world !"/>
+    <CompForm />
+    <p>Un paragraphe 3</p>
+>>>>>>> initbis
   </div>
 </template>
 
 <script>
+<<<<<<< HEAD
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -14,6 +27,21 @@ export default {
     HelloWorld
   }
 }
+=======
+import Composant2 from './components/Composant2.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import CompForm from './components/CompForm.vue'
+export default {
+  name: 'App',
+  components: {
+    Composant2,
+    HelloWorld,
+    CompForm
+  },
+
+}
+
+>>>>>>> initbis
 </script>
 
 <style>
---
+++ src/components/CompForm.vue
@@ -0,0 +1,6 @@
+<template>
+    <form method="GET">
+        <input name="nom">
+        <input type="Submit" value="Envoyer"/>
+    </form>
+</template>
